13088cf005d6c0f0b98391ce92eec5bdacec6198
Ben Garcia <ben@seebenprogram.com>
2018-04-24 12:12:48 -0400

    Refactor to define sequelize model

--- models/burger.js
+++ models/burger.js
@@ -1,34 +1,18 @@
-const orm = require('../config/orm.js');
-
-const burger = {
-  getAllBurgers: () => {
-    return new Promise((resolve, reject) => {
-      orm.selectAll()
-        .then(res => resolve(res))
-        .catch(err => reject(err));
-    });
-  },
-  addNewBurger: burger => {
-    return new Promise((resolve, reject) => {
-      orm.insertOne(burger)
-        .then(res => resolve(res))
-        .catch(err => reject(err));
-    });
-  },
-  eatBurger: burger => {
-    return new Promise((resolve, reject) => {
-      orm.updateOne(burger)
-        .then(res => resolve(res))
-        .catch(err => reject(err));
-    });
-  },
-  deleteAllBurgers: () => {
-    return new Promise((resolve, reject) => {
-      orm.deleteAll()
-        .then(res => resolve(res))
-        .catch(err => reject(err));
-    });
+module.exports = (sequelize, DataTypes) => {
+  const Burgers = sequelize.define("Burgers", {
+    burger_name: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        len: [1,64],
+        isAlpha: true
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, { 
+    timestamps: false 
   }
-};
-
-module.exports = burger;
+)};
