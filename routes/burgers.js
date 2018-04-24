658d2d6602fcf439c4ac2bd18d2ac678270ab7f1
Ben Garcia <ben@seebenprogram.com>
2018-04-24 19:06:13 -0400

    Start building post method

--- routes/burgers.js
+++ routes/burgers.js
@@ -10,7 +10,25 @@ apiRouter.route('/')
 
   // Create new burger (and customer if not already in DB)
   .post((req, res, next) => {
+    // First create new customer
+    models.Customer.findOrCreate({
+      where: {
+        customer_name: req.body.customer_name
+      }
+    })
+      .spread((user, created) => {
+        console.log(user.get({plain: true}).id);
+        res.status(201).send();
+      })
+      .then()
+      .catch(err => console.error(err));
+      /* .then(resp => {
 
+      }); */
+    // Then create burger
+    /* models.Burger.create({
+      burger_name: req.body.burger_name
+    }); */
   })
 
   // Update burger to 'devoured: true'
