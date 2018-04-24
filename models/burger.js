8e15bd4c05ddbbe37bf7b0bf16da8b33f862c805
Ben Garcia <ben@seebenprogram.com>
2018-04-24 17:39:25 -0400

    Update col name to conform to convention

--- models/burger.js
+++ models/burger.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
         isAlpha: true
       }
     },
-    isDevoured: {
+    is_devoured: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
     },
