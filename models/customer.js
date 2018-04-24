c63dd83432fdcedb5078f23387cf72060a6c11a6
Ben Garcia <ben@seebenprogram.com>
2018-04-24 12:32:52 -0400

    Build customer model

---
+++ models/customer.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Customers = sequelize.define("Customers", {
+    customer_name: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        len: [1, 64],
+        isAlpha: true
+      }
+    },
+  }, {
+      timestamps: false
+    }
+  )
+};
