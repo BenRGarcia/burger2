f575d7087e94620a75a126a1b03a43173d36c11d
Ben Garcia <ben@seebenprogram.com>
2018-04-25 10:40:26 -0400

    add comments

--- routes/burgers.js
+++ routes/burgers.js
@@ -16,44 +16,52 @@ apiRouter.route('/')
     // Create new burger with association to customer id
     models.Burger.create({
         burger_name,
-        Customer: {customer_name}
+        Customer: { customer_name }
       },{
         include: [{
           association: models.Burger.belongsTo(models.Customer)
         }]
       })
+      // Send response to client
       .then(() => res.status(201).send())
       .catch(err => {
         console.error(err);
         res.status(500).send();
-      })
+      });
   })
 
   // Update burger to 'devoured: true'
   .put((req, res, next) => {
-    const is_devoured = true;
+    // Parse out data from request body
     const id = req.body.id;
-    models.Burger.update({is_devoured},{
-      where: {id}
+    // Define data to update
+    const is_devoured = true;
+    // Query DB to update data
+    models.Burger.update({ is_devoured },{
+      where: { id }
     })
+      // Send response to client
       .then(() => res.status(204).send())
       .catch(err => {
         console.error(err);
         res.status(500).send();
-      })
+      });
   })
 
   // Delete all burgers and customers
   .delete((req, res, next) => {
+    // Delete Burger table
     models.Burger.destroy({ where: {} })
       .then(() => {
+        // Delete Customer table
         return models.Customer.destroy({ where: {} })
       })
+      // Send response to client
       .then(() => res.status(204).send())
       .catch(err => {
         console.error(err);
         res.status(500).send();
-      })
+      });
   });
 
 module.exports = apiRouter;
